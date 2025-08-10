f89d70ae73f9cde9419393665598cb0bde0dc08c
Alauddin-AlAzad <alaudinf92@gmail.com>
2025-08-11 03:36:35 +0600

    day-1 done

--- client/src/pages/JobDetails.jsx
+++ client/src/pages/JobDetails.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { AuthContext } from '../providers/AuthProvider'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { format } from 'date-fns'
 
 const JobDetails = () => {
   const [startDate, setStartDate] = useState(new Date())
+  const { id } = useParams();
+  const { user } = useContext(AuthContext)
+  const [job, setJob] = useState([])
+  useEffect(() => {
+    fetchJobData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  const fetchJobData = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/job/${id}`)
+    setJob(data)
+    setStartDate(new Date(data.deadline))
+  }
+  const { title, deadline, category, min_price, max_price, description } = job || {};
+
+  const handleformSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const price = form.price.value;
+    const email = form.email.value;
+    const comment = form.comment.value;
+    const bidFromData = {
+      price,
+      email,
+      comment
+    }
+
+  }
 
   return (
     <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
       {/* Job Details */}
       <div className='flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]'>
         <div className='flex items-center justify-between'>
-          <span className='text-sm font-light text-gray-800 '>
-            Deadline: 28/05/2024
-          </span>
+
+          {deadline && <span className='text-xs font-light text-gray-800 '>
+            Deadline: {format(new Date(deadline), 'P')}
+          </span>}
+
+
           <span className='px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full '>
-            Web Development
+
+            {category}
           </span>
         </div>
 
         <div>
           <h1 className='mt-2 text-3xl font-semibold text-gray-800 '>
-            Web Development
+            {title}
           </h1>
 
           <p className='mt-2 text-lg text-gray-600 '>
-            Dramatically redefine bleeding-edge infrastructures after
-            client-focused value. Intrinsicly seize user-centric partnerships
-            through out-of-the-box architectures. Distinctively.
+            {description}
           </p>
           <p className='mt-6 text-sm font-bold text-gray-600 '>
             Buyer Details:
@@ -35,10 +70,10 @@ const JobDetails = () => {
           <div className='flex items-center gap-5'>
             <div>
               <p className='mt-2 text-sm  text-gray-600 '>
-                Name: Programming-Hero Instructors
+                Name:{job?.buyer?.name}
               </p>
               <p className='mt-2 text-sm  text-gray-600 '>
-                Email: instructors@programming-hero.com
+                Email: {job?.buyer?.email}
               </p>
             </div>
             <div className='rounded-full object-cover overflow-hidden w-14 h-14'>
@@ -49,7 +84,7 @@ const JobDetails = () => {
             </div>
           </div>
           <p className='mt-6 text-lg font-bold text-gray-600 '>
-            Range: $500 - $600
+            Range: ${min_price} - ${max_price}
           </p>
         </div>
       </div>
@@ -59,7 +94,7 @@ const JobDetails = () => {
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleformSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='price'>
@@ -83,6 +118,7 @@ const JobDetails = () => {
                 type='email'
                 name='email'
                 disabled
+                defaultValue={user?.email}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
--- client/src/pages/UpdateJob.jsx
+++ client/src/pages/UpdateJob.jsx
@@ -1,9 +1,76 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import toast from 'react-hot-toast'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AuthContext } from '../providers/AuthProvider'
 
 const UpdateJob = () => {
+  const { id } = useParams();
+  const { user } = useContext(AuthContext)
   const [startDate, setStartDate] = useState(new Date())
+  const [job, setJob] = useState([])
+  useEffect(() => {
+    fetchJobData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  const fetchJobData = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/job/${id}`)
+    setJob(data)
+    setStartDate(new Date(data.deadline))
+  }
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    // Handle form submission logic here
+    const form = e.target;
+    const title = form.job_title.value;
+    const email = form.email.value;
+    const deadline = startDate;
+    const category = form.category.value;
+    const min_price = parseFloat(form.min_price.value);
+    const max_price = parseFloat(form.max_price.value);
+    const description = form.description.value;
+    const formData =
+    {
+      title,
+      buyer: {
+        email,
+        name: user?.displayName,
+        photo: user?.photoURL
+      },
+      deadline,
+      category,
+      min_price,
+      max_price,
+      description,
+      bid_count: 0,
+    };
+
+    // Make a post request
+    try {
+      await axios.put(`${import.meta.env.VITE_API_URL}/update-job/${id}`,
+        formData
+
+      )
+      toast.success('Data Updated Succesfully')
+      navigate('/my-posted-jobs')
+      form.reset();
+    }
+    catch (err) {
+      console.log(err)
+      toast.error('Invalid attempt')
+    }
+
+    // Reset form after submission
+    form.reset();
+    setStartDate(new Date());
+  }
 
   return (
     <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
@@ -12,7 +79,7 @@ const UpdateJob = () => {
           Update a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='job_title'>
@@ -22,6 +89,7 @@ const UpdateJob = () => {
                 id='job_title'
                 name='job_title'
                 type='text'
+                defaultValue={job.title}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
@@ -34,6 +102,7 @@ const UpdateJob = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={job.buyer?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
@@ -48,20 +117,23 @@ const UpdateJob = () => {
               />
             </div>
 
-            <div className='flex flex-col gap-2 '>
-              <label className='text-gray-700 ' htmlFor='category'>
-                Category
-              </label>
-              <select
-                name='category'
-                id='category'
-                className='border p-2 rounded-md'
-              >
-                <option value='Web Development'>Web Development</option>
-                <option value='Graphics Design'>Graphics Design</option>
-                <option value='Digital Marketing'>Digital Marketing</option>
-              </select>
-            </div>
+            {
+              job.category && <div className='flex flex-col gap-2 '>
+                <label className='text-gray-700 ' htmlFor='category'>
+                  Category
+                </label>
+                <select
+                  name='category'
+                  defaultValue={job.category}
+                  id='category'
+                  className='border p-2 rounded-md'
+                >
+                  <option value='Web Development'>Web Development</option>
+                  <option value='Graphics Design'>Graphics Design</option>
+                  <option value='Digital Marketing'>Digital Marketing</option>
+                </select>
+              </div>
+            }
             <div>
               <label className='text-gray-700 ' htmlFor='min_price'>
                 Minimum Price
@@ -70,6 +142,7 @@ const UpdateJob = () => {
                 id='min_price'
                 name='min_price'
                 type='number'
+                defaultValue={job.min_price}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
@@ -81,6 +154,7 @@ const UpdateJob = () => {
               <input
                 id='max_price'
                 name='max_price'
+                defaultValue={job.max_price}
                 type='number'
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
@@ -94,6 +168,7 @@ const UpdateJob = () => {
               className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               name='description'
               id='description'
+              defaultValue={job.description}
               cols='30'
             ></textarea>
           </div>
